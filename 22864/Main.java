4f482c7c9638346451bfafe731d1c34fe2da2435
Jinwoo Lee <binaryWooo@gmail.com>
2024-01-15 02:12:19 +0900

    date: 2024.01.13.

--- 22864/Main.java
+++ 22864/Main.java
@@ -1,5 +1,30 @@
-public class Main {
-    public static void main(String[] args) {
+import java.util.Scanner;
 
+public class Main {
+    static Scanner sc = new Scanner(System.in);
+
+    public static void main(String[] args) {
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
+        int m = sc.nextInt();
+        System.out.println(solution(a, b, c, m));
+    }
+
+    static int solution(int a, int b, int c, int m) {
+        int answer = 0;
+
+        int time = 0, stress = 0;
+        while (time < 24) {
+            ++time;
+            if (stress + a <= m) {
+                answer += b;
+                stress += a;
+            } else {
+                stress = stress - c <= 0 ? 0 : stress - c;
+            }
+        }
+
+        return answer;
     }
 }
